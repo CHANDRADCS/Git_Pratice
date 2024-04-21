c853c05d636df705b36bafe14cb205acec0b8b22
CHANDRA <chandrashakerdcs@gmail.com>
2024-04-21 12:58:25 +0530

    condition TRUE or FASLE

---
+++ condition.sh
@@ -0,0 +1,15 @@
+#! usr/bin/bash
+#
+#Author	: chandra
+#purpose: working
+#
+
+echo "type what you want to store"
+name="chandra"
+
+if [ "name = 'chandra'" ];
+then
+	echo "correct"
+else
+	echo "wrong"
+fi
